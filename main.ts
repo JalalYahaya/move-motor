8fcb6045cf8a92988a9e22e2802efb614f2ec56e
JalalYahaya <glalali7@gmail.com>
2024-11-04 13:28:01 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     led.plot(randint(0, 4), randint(0, 4))
-})
-basic.forever(function () {
-	
+    Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 10)
+    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 10)
+    Kitronik_Move_Motor.stop()
 })
